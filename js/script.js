767023eb5199c1a5e732fd6075741197b0673576
rakeshkhaarol <rakeshkharol457@gmail.com>
2022-11-29 22:10:24 +0530

    incriment & dicriment

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.write("<p>hello from extranal </p>");
+
+let g = 20;
+g++;
+g++;
+g++;
+g++;
+g++;
+g++;
+
+
+g--;
+g--;
+g--;
+g--;
+g--;
+g--;
+g--;
+g--;
+g--;
+g--;
+
+var k = g;
+
+window.alert( k)
---
+++ js/text.js
@@ -0,0 +1,27 @@
+console.log('hello node js')
+
+var a =20; 
+var b =30;
+
+
+console.log(a+b);
+
+let p =30;
+
+p++;
+p++;
+p++;
+p++;
+p++;
+p++;
+
+const l=p;
+
+console.log(l);
+
+const a1 =40;
+var a2 =21;
+
+console.log(a1+a2)
+
+
